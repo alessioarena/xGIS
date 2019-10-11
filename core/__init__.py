7d39351d015df8068b45e0af7933abb4881150c5
Alessio Arena <are014@csiro.au>
2019-10-11 11:24:43 +1100

    add logger on import

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,6 @@
 from ._version import __version__
 from .executor import Executor, ExternalExecutionError
+from . import xGISlogger as log_utils
 
 version = __version__
 version_info = tuple([int(v) for v in __version__.split('.')])
